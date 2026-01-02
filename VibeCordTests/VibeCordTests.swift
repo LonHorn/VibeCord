d9bc5bdc2be3555c95a81a4dd78a04382c5dfa9e
Jelly <jellylopster@gmail.com>
2026-01-02 17:05:20 +0100

    Initial Commit

---
+++ VibeCordTests/VibeCordTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCordTests.swift
+//  VibeCordTests
+//
+//  Created by Александр Гарбушка on 02/01/2026.
+//
+
+import Testing
+@testable import VibeCord
+
+struct VibeCordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
